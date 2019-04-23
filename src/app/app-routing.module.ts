082aa43cc1f08a9aa7eace12d1f3e730536c45ff
LangJa <lange@infoboard.biz>
2019-04-23 16:44:33 +0200

    add Pages (calendar, chat, help, settings)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,11 @@ const routes: Routes = [
         loadChildren: './loading/loading.module#LoadingPageModule'
     },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' }
+  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' },  { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
+  { path: 'help', loadChildren: './help/help.module#HelpPageModule' },
+  { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' }
+
 ];
 
 @NgModule({
