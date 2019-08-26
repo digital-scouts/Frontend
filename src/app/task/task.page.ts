59e3f404b36340261534584faba84066a2bddad8
LangJa <lange@infoboard.biz>
2019-08-26 17:32:20 +0200

    add task list

--- src/app/task/task.page.ts
+++ src/app/task/task.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpServiceService} from '../http-service.service';
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.page.html',
-  styleUrls: ['./task.page.scss'],
+    selector: 'app-task',
+    templateUrl: './task.page.html',
+    styleUrls: ['./task.page.scss'],
 })
 export class TaskPage implements OnInit {
 
-  constructor() { }
+    tasks: Array<{
+        report: Array<string>,
+        done: boolean,
+        competent: Array<{
+            image_profile: string,
+            _id: string,
+            name_first: string,
+            name_last: string,
+            email: string,
+            role: string,
+        }>,
+        _id: string,
+        title: string,
+        description: string,
+        dueDate: Date,
+    }>;
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpServiceService) {
+    }
+
+    ngOnInit() {
+        this.http.getAllTask().then((data) => {
+            // @ts-ignore
+          this.tasks = data;
+        });
+    }
 
 }
